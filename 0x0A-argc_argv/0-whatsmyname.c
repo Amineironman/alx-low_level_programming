441d7a5e2af1620297624aa10fef6b5fbe0faf7f
Amineironman <elhaoudaramine8@gmail.com>
2023-05-01 07:50:44 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[])
-{
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
-}
 /**
 * main - entry point
 * @argc: contain number of arguments
 * @argv: pointer of pointers containning strings
 * Return: 0 on success
 */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
