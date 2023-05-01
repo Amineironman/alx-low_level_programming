f804baea7c0b308c2cf8b688072ead1c80d15870
Amineironman <elhaoudaramine8@gmail.com>
2023-05-01 07:54:06 -0700

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
-
-{
-	(void) argv;
-	printf("%d\n", argc - 1);
-	return (0);
-}
 /**
 * main - entry point
 * @argc: number of command line arguments
 * @argv: array containing program line arguments
 * Return: 0 on success
 */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
