675be597658bf78d14834bb5c6f4e96c4e94cd2b
Amineironman <elhaoudaramine8@gmail.com>
2023-05-02 16:54:18 -0700

    all tasks

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
 int pop_listint(listint_t **head)
 {
 	int hnode;
@@ -20,11 +28,3 @@ int pop_listint(listint_t **head)
 
 	return (hnode);
 }
-
-/**
- * pop_listint - deletes the head node of
- * a linked list
- * @head: head of a list.
- *
- * Return: head node's data.
- */
