05c7218691d7d34701783b60cdaf5ce878fa6740
Amineironman <elhaoudaramine8@gmail.com>
2023-05-02 16:19:30 -0700

    all tasks

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,19 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-{
-	void *p;
-	struct listp_s *next;
-} listp_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -33,7 +20,18 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 /**
  * struct listint_s - singly linked list
@@ -49,3 +47,4 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+#endif
