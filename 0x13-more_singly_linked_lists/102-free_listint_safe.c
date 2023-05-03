29b64df2c9f4433d378fe9971de412a4a346f4b4
Amineironman <elhaoudaramine8@gmail.com>
2023-05-02 17:08:23 -0700

    all tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * free_listint_safe - frees a linked list.
  * @h: head of a list.
  *
@@ -46,27 +69,4 @@ size_t free_listint_safe(listint_t **h)
 	*h = NULL;
 	free_listp2(&hptr);
 	return (nnodes);
-}
-
-/**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
 }
